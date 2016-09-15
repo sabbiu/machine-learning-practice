c5e05ab8169dd550225d5317d9c6af1caa0b20c1
sabbiu <070bct531@ioe.edu.np>
2016-09-15 08:23:49 -0400

    Tut 2 completed

--- iris_flower.py
+++ iris_flower.py
@@ -28,3 +28,18 @@ clf = clf.fit(train_data, train_target)
 print(test_target)
 #output predicted
 print(clf.predict(test_data))
+
+
+#visualization code
+from sklearn.externals.six import StringIO
+import pydotplus
+  
+dot_data = StringIO()  
+tree.export_graphviz(clf, out_file=dot_data,  
+                         feature_names=iris.feature_names,  
+                         class_names=iris.target_names,  
+                         filled=True, rounded=True,  
+                         impurity=False)  
+graph = pydotplus.graph_from_dot_data(dot_data.getvalue())  
+graph.write_pdf("iris.pdf")
+
