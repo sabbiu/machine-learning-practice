4844fc8df7c51b92e15d2a64761ae718cca12787
sabbiu <070bct531@ioe.edu.np>
2016-09-15 11:48:16 -0400

    Our own classifier code

--- pipeline.py
+++ pipeline.py
@@ -1,3 +1,41 @@
+import random
+from scipy.spatial import distance
+def euc(a,b):
+	return distance.euclidean(a,b)
+
+class ScrappyKNN():
+	def fit(self, X_train, y_train):
+		self.X_train = X_train
+		self.y_train = y_train
+
+	#random prediction
+	#def predict(self, X_test):
+	#	predictions = []
+	#	for row in X_test:
+	#		label = random.choice(self.y_train)
+	#		predictions.append(label)
+#
+#		return predictions
+
+	def predict(self, X_test):
+		predictions = []
+		for row in X_test:
+			label = self.closest(row)
+			predictions.append(label)
+
+		return predictions
+
+	def closest(self, row):
+		best_dist = euc(row, self.X_train[0])
+		best_index = 0
+		for i in range(1,len(self.X_train)):
+			dist = euc(row, self.X_train[i])
+			if dist < best_dist:
+				best_dist = dist
+				best_index = i
+
+		return self.y_train[best_index]
+
 from sklearn import datasets
 iris = datasets.load_iris()
 
@@ -34,3 +72,16 @@ my_predictions = my_clf.predict(X_test)
 
 #accuracy calculations
 print(accuracy_score(y_test, my_predictions))
+print()
+
+
+#our own classifier
+print('KNN OUR->'),
+our_clf = ScrappyKNN()
+our_clf.fit(X_train,y_train)
+
+our_predictions = our_clf.predict(X_test)
+#print(predictions)
+
+#accuracy calculations
+print(accuracy_score(y_test, our_predictions))
